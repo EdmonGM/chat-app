95048ce5d4e8fc60dfd43c246dadf0f49dd10f52
Edmon <ed.moussa4@gmail.com>
2025-04-30 16:33:17 +0300

    added auth logic

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,76 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import prisma from "@/lib/prisma";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "Enter Your Email",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Enter Your Password",
+        },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials.password) {
+          throw new Error("Email and password are required");
+        }
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        if (!user) {
+          const hashedPassword = await bcrypt.hash(credentials.password, 10);
+          const newUser = await prisma.user.create({
+            data: {
+              email: credentials.email,
+              name: credentials.email.split("@")[0],
+              password: hashedPassword,
+            },
+          });
+          return newUser;
+        }
+
+        const isValidPassword = await bcrypt.compare(
+          credentials.password,
+          user.password!
+        );
+
+        if (!isValidPassword) {
+          throw new Error("Invalid password");
+        }
+        return user;
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.email = user.email;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id;
+        session.user.email = token.email;
+      }
+      return session;
+    },
+  },
+  debug: process.env.NODE_ENV === "development",
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
