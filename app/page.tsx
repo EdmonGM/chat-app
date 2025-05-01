1ab947154d8aa385deb4ad6121989132af88097d
Edmon <ed.moussa4@gmail.com>
2025-05-01 15:16:19 +0300

    Created landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,19 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <p>Hello World!</p>;
+  return (
+    <section className="flex flex-col justify-center items-center w-full h-screen">
+      <Image
+        src="/logo.svg"
+        alt="logo"
+        width={100}
+        height={100}
+        draggable={false}
+      />
+      <p className="w-1/2 text-center">
+        Welcome to Chat App! A full-stack realtime chat application built with
+        Next.js.
+      </p>
+    </section>
+  );
 }
