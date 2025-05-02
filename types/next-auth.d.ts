dfca1b9e5453b17840ffd87ae1302f1047dbd577
Edmon <ed.moussa4@gmail.com>
2025-05-02 23:47:33 +0300

    Added friends interface

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -25,3 +25,19 @@ export interface IUser {
   name: string;
   avatar: string | null;
 }
+export interface IFriendRequest {
+  receiverId: string;
+  senderId: string;
+  sender: {
+    name: string;
+    username: string;
+    avatar: string | null;
+  };
+  sentAt: string;
+}
+export interface IFriend {
+  id: string;
+  username: string;
+  name: string;
+  avatar: string | null;
+}
