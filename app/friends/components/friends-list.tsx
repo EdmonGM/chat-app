50b76f88dcaddee8f204b45279ceb71d359d020b
Edmon <ed.moussa4@gmail.com>
2025-05-03 17:26:31 +0300

    Implemented GET friends request

--- app/friends/components/friends-list.tsx
+++ app/friends/components/friends-list.tsx
@@ -2,44 +2,64 @@
 import React from "react";
 import { Card, CardContent } from "@/components/ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { useQuery } from "@tanstack/react-query";
-import { getAllFriends } from "@/lib/api";
-import { useSession } from "next-auth/react";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { createChatRoom, getAllFriends } from "@/lib/api";
 import LoadingLogo from "@/components/ui/logo";
+import { MessageSquareMore, UserMinus } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 function FriendsList() {
-  const { data: session } = useSession();
   const { data: friends = [], isFetching } = useQuery({
     queryKey: ["users", "requests"],
-    queryFn: () => getAllFriends(session?.user.id!),
+    queryFn: () => getAllFriends(),
   });
-  console.log(friends);
+
+  const router = useRouter();
+
+  const { mutateAsync } = useMutation({
+    mutationKey: ["chats"],
+    mutationFn: (secondUserId: string) => createChatRoom(secondUserId),
+    onSuccess: (chat) => {
+      router.push(`/chat/${chat.id}`);
+    },
+  });
 
   if (isFetching) return <LoadingLogo />;
 
   if (friends?.length < 1) return <></>;
 
   return (
-    <div>
+    <section className="my-4">
+      <h1 className="text-xl mb-4">Friends List</h1>
       {friends.map((friend) => {
         return (
-          <Card className="cursor-pointer" key={friend.id}>
-            <CardContent className="flex items-center gap-8">
-              <Avatar>
-                <AvatarImage src={friend.avatar!} />
-                <AvatarFallback className="text-xl select-none">
-                  {friend.name.slice(0, 2)}
-                </AvatarFallback>
-              </Avatar>
-              <div>
+          <Card key={friend.id}>
+            <CardContent className="flex items-center justify-between">
+              <div className="flex items-center gap-8">
+                <Avatar>
+                  <AvatarImage src={friend.avatar!} />
+                  <AvatarFallback className="text-xl select-none">
+                    {friend.name.slice(0, 2)}
+                  </AvatarFallback>
+                </Avatar>
                 <h1>{friend.name}</h1>
-                <p>last message</p>
+              </div>
+              <div className="flex items-center gap-4">
+                <MessageSquareMore
+                  color="var(--primary)"
+                  className="cursor-pointer"
+                  onClick={async () => await mutateAsync(friend.id)}
+                />
+                <UserMinus
+                  color="var(--destructive)"
+                  className="cursor-pointer"
+                />
               </div>
             </CardContent>
           </Card>
         );
       })}
-    </div>
+    </section>
   );
 }
 
